557e95824448a5f8b8867418b999e900c0981514
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-10 11:38:47 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+const receivesAFunction = (callback) =>{
+ callback();
+}
+
+const returnsANamedFunction = () => {
+    return function funtName(){
+}
+}
+
+
+const returnsAnAnonymousFunction = () => {
+return function (){
+} 
+}
+
+    
+
+      
